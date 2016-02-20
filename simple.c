1b17d7ebfd40266e2cc390fe3c222df0d95e9e35
Kazami <kazamizero@gmail.com>
2016-02-20 18:55:41 +0800

    title: simple edited what: add the small program

--- simple.c
+++ simple.c
@@ -7,7 +7,12 @@ int token;
 char *src, *old_src;
 int poolsize;
 int line;
-
+int *text,
+    *old_text,
+    *stack;
+char *data;
+int *pc, *bp, *sp, ax, cycle;
+enum { ADD, PUSH, LI, LC, SI, SC, IMM, EXIT};
 void next(){
     token = *src++;
     return;
@@ -22,8 +27,24 @@ void program(){
 };
 
 int eval(){
+    int op, *tmp;
+    while (1) {
+        op = *pc++;
+        printf("%d, %p, %p\n", ax, (char *)ax, (int *)ax);
+        if (op == IMM) { ax = *pc++;}
+        else if (op == LC) { ax = *(char *)ax; }
+        else if (op == LI) { ax = *(int *)ax; }
+        else if (op == SC) { ax = *(char *)*sp++ = ax; }
+        else if (op == SI) { *(int *)*sp++ = ax; }
+        else if (op == PUSH) {*--sp = ax; }
+        else if (op == ADD) ax = *sp++ + ax; 
+        else if (op == EXIT) {printf("exit(%d)", *sp); return *sp;}
+        else {
+            printf("unknown instruction:%d\n", op);
+        }
+    }
     return 0;
-};
+}
 
 int main(int argc, char **argv) {
     int i, fd;
@@ -48,9 +69,46 @@ int main(int argc, char **argv) {
         printf("read() returned %d\n", i);
         return -1;
     }
-    src[i] = 0;
-    close(fd);
+    if (!(text = old_text = malloc(poolsize))) {
+        printf("could not malloc(%d) for text area\n", poolsize);
+        return -1;
+    }
+    if (!(data = malloc(poolsize))) {
+        printf("could not malloc(%d) for data area\n", poolsize);
+        return -1;
+    }
 
+    if (!(stack = malloc(poolsize))) {
+        printf("could not malloc(%d) for stack area\n", poolsize);
+        return -1;
+    }
+
+    memset(stack, 0, poolsize);
+    memset(text, 0, poolsize);
+    memset(data, 0, poolsize);
+    bp = sp = (int *)((int)stack + poolsize);
+    ax = 0;
+
+    i = 0;
+    text[i++] = IMM;
+    text[i++] = 10;
+    text[i++] = PUSH;
+    text[i++] = IMM;
+    text[i++] = 20;
+    text[i++] = ADD;
+    text[i++] = PUSH;
+    text[i++] = EXIT;
+    pc = text;
+    int sum;
+    int a = 255;
+    char x = 'c';
+    int *b;
+    b = &x;
+    sum = *b;
+    sum = *(int *)&sum;
+    printf("%d\n", sum);
+    sum = *(char *)&sum;
+    printf("%c\n", sum);
     program();
     return eval();
     
